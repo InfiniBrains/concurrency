556e465358c84db540f0046b4d838524114c685a
Alexandre Tolstenko <tolstenko@gmail.com>
2022-12-12 21:39:39 -0500

    pre-js fix

--- cmake/pre.js
+++
@@ -1,5 +0,0 @@
-// todo: move this to a better place
-// ugly hack for crossOriginIsolated not enabled and SharedArrayBuffer not available
-if (typeof (window) == 'object'
-    && (typeof (crossOriginIsolated) == 'undefined' || !crossOriginIsolated))
-  SharedArrayBuffer = ArrayBuffer;
